20a3b6bc730c2a2cd805f0a98d71e970a378ff40
ilya ivanov <ilyushaivanov@gmail.com>
2026-06-18 18:37:12 +0300

    review

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -179,9 +179,8 @@ class RecipeIngredient(models.Model):
         ]
 
     def __str__(self):
-        return f'{self.ingredient.name} – {self.amount} {
-            self.ingredient.measurement_unit
-        }'
+        return (f'{self.ingredient.name} – {self.amount} '
+            f'{self.ingredient.measurement_unit}')
 
 
 class Favorite(models.Model):
