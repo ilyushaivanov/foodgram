19e780374c4a9a53605574d9e4b35f53fd0fbeb7
ilya ivanov <ilyushaivanov@gmail.com>
2026-06-18 18:30:51 +0300

    review

--- backend/api/views.py
+++ backend/api/views.py
@@ -135,21 +135,27 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if self.action in ('create', 'update', 'partial_update'):
             return RecipeCreateUpdateSerializer
         return RecipeSerializer
-    
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         recipe = serializer.save(author=request.user)
-        output_serializer = RecipeSerializer(recipe, context={'request': request})
+        output_serializer = RecipeSerializer(
+            recipe, context={'request': request}
+        )
         return Response(output_serializer.data, status=status.HTTP_201_CREATED)
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=partial
+        )
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
-        output_serializer = RecipeSerializer(instance, context={'request': request})
+        output_serializer = RecipeSerializer(
+            instance, context={'request': request}
+        )
         return Response(output_serializer.data)
 
     @action(detail=True, methods=['get'], url_path='get-link')
