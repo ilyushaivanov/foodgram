6a7f49b5ae0485afa7b909a9737b6f47e40fd5e2
ilya ivanov <ilyushaivanov@gmail.com>
2026-07-01 13:41:01 +0300

    pagination

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -60,7 +60,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'PAGE_SIZE_QUERY_PARAM': 'limit',
     'DEFAULT_FILTER_BACKENDS': [
